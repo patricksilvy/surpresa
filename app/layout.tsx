32a1280c0c99d3f1d632285899a57ffd24381950
patricksilvy <pat28910oshi@gmail.com>
2024-09-24 11:00:57 -0300

    Erro fix 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,21 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
 export const metadata: Metadata = {
-  title: "Aprendizado sempre!",
-  description: "Aproveite o mês de outubro para mudar sua vida!",
+  title: "Surpresa!",
+  description: "Mude sua vida em outubro!",
 };
 
 export default function RootLayout({
@@ -14,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
